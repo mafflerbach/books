5339a2cc60821496c05b6cf18c5e22b5465e1f44
mafflerbach <maren@afflerbach.info>
2014-02-14 21:47:18 +0100

    Initial commit

---
+++ create.cmd
@@ -0,0 +1,18 @@
+@echo off
+TITLE MakeDocBook
+
+REM ==============================================================
+REM Edit these lines:
+SET scriptpath=E:\xampp\htdocs\books\vendor
+set src=E:\xampp\htdocs\books\output
+
+SET stylepath=%scriptpath%\docbook-xsl-1.78.1\epub3
+SET foppath=%scriptpath%\fop
+set repository=%scriptpath%\repository
+set fooutput=%src%\fo
+set pdfoutput=%src%\pdf
+set xsltproc_home=%scriptpath%\libxslt-1.1.26.win32\bin
+REM ==============================================================
+
+%xsltproc_home%\xsltproc -o %fooutput%\ebook1\OEBPS %stylepath%\chunk.xsl %src%\src\%1.xml
+%scriptpath%\zip\zip.exe -r -X %fooutput%\mybook.epub %fooutput%\ebook1\mimetype %fooutput%\ebook1\META-INF %fooutput%\ebook1\OEBPS
---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Mozilla Rich Text Editing Demo</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+    <link rel="stylesheet" href="css/jquery-te-1.4.0.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <script src="js/jquery-te-1.4.0.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="js/themes/black/easyui.css">
+    <link rel="stylesheet" type="text/css" href="js/themes/icon.css">
+    <script src="js/jquery.easyui.min.js"></script>
+
+</head>
+<body>
+<div class="bookmenu">
+    <ul id="tt" class="easyui-tree" ></ul>
+</div>
+<div class="editor">
+    <textarea id="editor"></textarea>
+</div>
+
+<div id="mm" class="easyui-menu" style="width:120px;">
+    <div onclick="append()" data-options="iconCls:'icon-add'">Append</div>
+    <div onclick="removeit()" data-options="iconCls:'icon-remove'">Remove</div>
+    <div onclick="rename()" data-options="iconCls:'icon-edit'">rename</div>
+    <div class="menu-sep"></div>
+    <div onclick="expand()">Expand</div>
+    <div onclick="collapse()">Collapse</div>
+</div>
+
+<script src="custom.js"></script>
+
+
+</body>
+</html>
