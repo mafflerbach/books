7fa5831d9f3a0895eecf7d90bb6101b5faed8259
mafflerbach <maren@afflerbach.info>
2014-02-22 18:34:47 +0100

    Update initdb.sql

--- initdb.sql
+++ initdb.sql
@@ -53,5 +53,6 @@ CREATE TABLE IF NOT EXISTS `sections` (
   `chapterid` int(11) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `sort` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
