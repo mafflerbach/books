4d1a1b15fd05e5c6f01e83065cee231a4795df34
mafflerbach <maren@afflerbach.info>
2014-02-23 00:02:57 +0100

    little refactoring and batch file fix

--- create.cmd
+++ create.cmd
@@ -11,7 +11,7 @@ set fooutput=%src%\%1%
 set xsltproc_home=%scriptpath%\libxslt-1.1.26.win32\bin
 REM ==============================================================
 #%xsltproc_home%\xsltproc --stringparam section.autolabel 0 --stringparam chapter.autolabel 0 -o %fooutput%\OEBPS %stylepath%\chunk.xsl %src%\%1.xml
-%xsltproc_home%\xsltproc --stringparam section.autolabel 0 --stringparam chapter.autolabel 0 -o %fooutput%\OEBPS %stylepath%\chunk.xsl %src%\%1.xml
+%xsltproc_home%\xsltproc --stringparam toc.section.depth 0 -o %fooutput%\OEBPS %stylepath%\chunk.xsl %src%\%1.xml
 
 set out=tmp\%1%
 set me=tmp\%1%.epub
