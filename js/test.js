34715fdf84a4d91f17d46dcc49ac847145e46ac5
mafflerbach <maren@afflerbach.info>
2014-03-10 08:34:18 +0100

    begin to remove easy ui

---
+++ js/test.js
@@ -0,0 +1,7 @@
+[
+  {"0": {"text": "Jellyfish.jpg"}, "me": {"0": {"text": "Hydrangeas.jpg"}, "mu": [
+    {"text": "Hydrangeas2.jpg"}
+  ], "1": {"text": "Tulips.jpg"}}, "mu": {"0": {"text": "Hydrangeas2.jpg"}, "me": {"0": {"text": "Hydrangeas.jpg"}, "mu": [
+    {"text": "Hydrangeas2.jpg"}
+  ], "1": {"text": "Tulips.jpg"}}}, "1": {"text": "Penguins.jpg"}, "2": {"text": "Tulips.jpg"}}
+]
