ab11a7317c35cac4ff6549487877a878221d6c54
mafflerbach <afflerbach@dimensional.de>
2014-04-08 14:45:48 +0200

    start settings for user

--- lib/Content/Page/Settings.php
+++ lib/Content/Page/Settings.php
@@ -24,8 +24,9 @@ class Settings {
     $this->addFormField($form, 'name', array('id'=>'name', 'name'=>'name', 'type'=>'text'), 'Name');
     $this->addFormField($form, 'surname', array('id'=>'surname', 'name'=>'surname', 'type'=>'text'), 'Surname');
     $this->addFormField($form, 'email', array('id'=>'email', 'name'=>'email', 'type'=>'text'), 'E-Mail');
-    $form->appendElement('label', array('for' =>'about'));
+    $form->appendElement('label', array('for' =>'about'), 'About me');
     $form->appendElement('textarea', array('id'=>'about', 'name' =>'content'));
+    $form->appendElement('br');
     $form->appendElement('button', array('id'=>'sendUserdata', 'name' =>'userdata'), 'save');
   }
 
@@ -33,6 +34,7 @@ class Settings {
     $form = $d->appendElement('form');
     $this->addFormField($form, 'email', array('id'=>'password', 'name'=>'password', 'type'=>'password'), 'Password');
     $this->addFormField($form, 'email', array('id'=>'repassword', 'name'=>'repassword', 'type'=>'password'), 're Password');
+    $form->appendElement('br');
     $form->appendElement('button', array('id'=>'changePass', 'name' =>'changePass'), 'change Password');
   }
 
