f1f3b431f11066df0ff2c3bc02a5cda0e55ec419
mafflerbach <afflerbach@dimensional.de>
2014-04-22 13:18:57 +0200

    add listitem

--- test.php
+++ test.php
@@ -2,26 +2,21 @@
 
   $diff
     = '
-diff --git a/Section_1-31-105.xml b/Section_1-31-105.xml
-index 834ad72..9f57218 100644
---- a/Section_1-31-105.xml
-+++ b/Section_1-31-105.xml
-@@ -3 +3,2 @@
-{+<para>+}Lorem pisstum{+ Das ist ein weiterer test</para>+}
-
-diff --git a/Section_2-31-106.xml b/Section_2-31-106.xml
-index 3ad50b7..047bcaa 100644
---- a/Section_2-31-106.xml
-+++ b/Section_2-31-106.xml
-@@ -4 +3,0 @@
-[-<para>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod</para>-]
-diff --git a/section_1-32-107.xml b/section_1-32-107.xml
-index a667960..02222b3 100644
---- a/section_1-32-107.xml
-+++ b/section_1-32-107.xml
+diff --git a/Neuer_Prozess-15-21.xml b/Neuer_Prozess-15-21.xml
+index d17875a..8dd23a1 100644
+--- a/Neuer_Prozess-15-21.xml
++++ b/Neuer_Prozess-15-21.xml
 @@ -5,2 +5,2 @@
-<para>asdfkja sfjaölsjdf aslködj asdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködj[-asdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködj-]</para>
-<para>asdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködj[-asdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködjasdfkja sfjaölsjdf aslködj-]</para>
+<para>Dies bedeutet , das ein zweiter Entwickler eine Review über den erzeugten code macht. Er gibt dem ersten Entwickler Feedback, ob er noch Änderungen vornehmen muss, oder ob die Tests ausreichend vollständig gemacht wurden. Der zweite Entwickler übernimmt  [-&nbsp;eine-]{+eine+} Teilverantwortung an dem umgesetzten Feature. Dieses vorgehen bringt uns zudem einen entscheidenden Nebeneffekt - <emphasis role="underline">Jeder Entwickler kennt jedes Projekt</emphasis>. So können zuvor Tester eines Projekte recht effizient in ein ihm bekanntes Projekt eingesetzt werden, wenn der jeweilige Stammentwickler ausgefallen ist, oder er anderweitig in einem Projekt gebunden ist.</para>
+<para>Durch die Implementierung des 4 Augenprizips werden so die Akzeptanz der funktionalen Tests erzwungen, und weitergehen die Qualität der Unit-Test [-erhöht.&nbsp;</para>-]{+erhöht. </para>+}
+diff --git a/Schulungen-15-24.xml b/Schulungen-15-24.xml
+index ef82e25..58095d3 100644
+--- a/Schulungen-15-24.xml
++++ b/Schulungen-15-24.xml
+@@ -4,2 +4,2 @@
+<para>Die Entwickler müssen noch darin geschult werden ihr Augenmerk auch auf die funktionalen Tests zu richten. Er muss wissen wann er im laufe der Entwicklung die Screenshot-Tests erstellen muss. Wo die Referenzbilder abgelegt werden müssen. Wann werden die anderen funktionalen Tests erstellt, und werden wie [-abgelegt.&nbsp;</para>-]{+abgelegt. </para>+}
+<para>Dies alles soll in einem Workshop in kleinen Gruppen vermittelt werden. Derzeit sind es etwa acht Entwickler die Schulungsbedarf haben. Vorgeschlagen wird, das in jeweils vierer Gruppen geschult wird. So ist ausreichend Raum für fragen und antworten.  [-&nbsp;</para>-]{+</para>+}
+
 ';
 
 $numstat = '2	1	Section_1-31-105.xml
@@ -87,6 +82,10 @@ function parseNumStat($numstat) {
     $line = str_replace('</section>', '', $line);
     $line = str_replace('<title>', '', $line);
     $line = str_replace('</title>', '', $line);
+    $line = str_replace('<itemizedlist>', '', $line);
+    $line = str_replace('</itemizedlist>', '', $line);
+    $line = str_replace('<listitem>', '', $line);
+    $line = str_replace('</listitem>', '', $line);
 
     return $line;
   }
